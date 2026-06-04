e96507971741d0c8921631513d21a705d4206c00
Marine <marineslimane05@gmail.com>
2026-06-04 14:41:20 +0200

    working on train face implementation

--- TD07_Train/train.cpp
+++ TD07_Train/train.cpp
@@ -6,7 +6,7 @@ STP3D::IndexedMesh* meshCylinder;
 
 void initFace()
 {
-    meshSphere = STP3D::basicSphere(5.4f, 60, 60); // radius, smoothness of vertical lines, smoothness of horizontal lines
+    meshSphere = STP3D::basicSphere(5.8f, 60, 60); // radius, smoothness of vertical lines, smoothness of horizontal lines
     meshSphere->createVAO();
 
     meshCylinder = STP3D::basicCylinder(30, 5, 60, 1); // height, radius, div_round, div_height
@@ -27,7 +27,7 @@ void drawFace(GLBI_Engine& myEngine)
 
     // base sphere of face (for round appearance of face, especially on the side)
     myEngine.mvMatrixStack.pushMatrix();
-    myEngine.mvMatrixStack.addTranslation(Vector3D(0, 8.0f, 10.0f));
+    myEngine.mvMatrixStack.addTranslation(Vector3D(0, 7.0f, 10.0f));
     // myEngine.mvMatrixStack.addHomothety(Vector3D(sr, 10.0f, sr));
     myEngine.updateMvMatrix();
     myEngine.setFlatColor(0.5, 0.5, 0.5);
@@ -37,7 +37,7 @@ void drawFace(GLBI_Engine& myEngine)
 
     // top black cylinder on neck
     myEngine.mvMatrixStack.pushMatrix();
-    myEngine.mvMatrixStack.addTranslation(Vector3D(0, 6.0f, 10.0f));
+    myEngine.mvMatrixStack.addTranslation(Vector3D(0, 6.5f, 10.0f));
     myEngine.mvMatrixStack.addHomothety(Vector3D(1.2, 1.2, 1.2));
     myEngine.updateMvMatrix();
     myEngine.setFlatColor(0.f, 0.f, 0.f);
@@ -45,13 +45,53 @@ void drawFace(GLBI_Engine& myEngine)
     myEngine.mvMatrixStack.popMatrix();
     myEngine.updateMvMatrix();
 
-    // eyes
-    // myEngine.mvMatrixStack.pushMatrix();
-    // myEngine.mvMatrixStack.addTranslation(Vector3D(5.0, 5.0f, 10.0f));
-    // // myEngine.mvMatrixStack.addHomothety(Vector3D(sr, 10.0f, sr));
-    // myEngine.updateMvMatrix();
-    // myEngine.setFlatColor(0.5, 0.5, 0.5);
-    // meshSphere->draw();
-    // myEngine.mvMatrixStack.popMatrix();
-    // myEngine.updateMvMatrix();
+    // EYES
+    // left base
+    myEngine.mvMatrixStack.pushMatrix();
+    myEngine.mvMatrixStack.addTranslation(Vector3D(-2.0, 3.0f, 11.0f));
+    myEngine.mvMatrixStack.addHomothety(Vector3D(0.35, 0.35, 0.35));
+    myEngine.updateMvMatrix();
+    myEngine.setFlatColor(1.0, 1.0, 1.0);
+    meshSphere->draw();
+    myEngine.mvMatrixStack.popMatrix();
+    myEngine.updateMvMatrix();
+    // right base
+    myEngine.mvMatrixStack.pushMatrix();
+    myEngine.mvMatrixStack.addTranslation(Vector3D(2.0, 3.0f, 11.0f));
+    myEngine.mvMatrixStack.addHomothety(Vector3D(0.35, 0.35, 0.35));
+    myEngine.updateMvMatrix();
+    myEngine.setFlatColor(1.0, 1.0, 1.0);
+    meshSphere->draw();
+    myEngine.mvMatrixStack.popMatrix();
+    myEngine.updateMvMatrix();
+    // right pupil
+    myEngine.mvMatrixStack.pushMatrix();
+    myEngine.mvMatrixStack.addRotation(-M_PI/120, Vector3D(0.0, 0.1, 0.0));
+    myEngine.mvMatrixStack.addTranslation(Vector3D(2.5, 2.0f, 10.5f));
+    myEngine.mvMatrixStack.addHomothety(Vector3D(0.225, 0.225, 0.225));
+    myEngine.updateMvMatrix();
+    myEngine.setFlatColor(0.0, 0.0, 0.0);
+    meshSphere->draw();
+    myEngine.mvMatrixStack.popMatrix();
+    myEngine.updateMvMatrix();
+    // left pupil
+    myEngine.mvMatrixStack.pushMatrix();
+    myEngine.mvMatrixStack.addRotation(M_PI/120, Vector3D(0.0, 0.1, 0.0));
+    myEngine.mvMatrixStack.addTranslation(Vector3D(-2.5, 2.0f, 10.5f));
+    myEngine.mvMatrixStack.addHomothety(Vector3D(0.225, 0.225, 0.225));
+    myEngine.updateMvMatrix();
+    myEngine.setFlatColor(0.0, 0.0, 0.0);
+    meshSphere->draw();
+    myEngine.mvMatrixStack.popMatrix();
+    myEngine.updateMvMatrix();
+    // right pupil
+    myEngine.mvMatrixStack.pushMatrix();
+    // myEngine.mvMatrixStack.addRotation(-M_PI/120, Vector3D(0.0, 0.1, 0.0));
+    myEngine.mvMatrixStack.addTranslation(Vector3D(2.5, 1.0f, 11.0f));
+    myEngine.mvMatrixStack.addHomothety(Vector3D(0.15, 0.15, 0.15));
+    myEngine.updateMvMatrix();
+    myEngine.setFlatColor(0.8, 0.8, 0.8);
+    meshSphere->draw();
+    myEngine.mvMatrixStack.popMatrix();
+    myEngine.updateMvMatrix();
 }
